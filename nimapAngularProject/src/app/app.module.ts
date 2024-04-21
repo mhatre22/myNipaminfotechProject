9473f2ae4184aa616d08320d94c07953b69a79ac
mhatre22 <mhatresnehal75@gmail.com>
2024-04-22 01:14:36 +0530

    footercommit

--- nimapAngularProject/src/app/app.module.ts
+++ nimapAngularProject/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserregisterComponent } from './userregister/userregister.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserregisterComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
